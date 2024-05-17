63afcbd3ec231027b019a5ed7b4ac6f2dc6f5422
nawfs254 <19b.cse254@rimt.ac.in>
2024-05-17 17:15:00 +0600

    hide credentials

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://arnob25086:JYgeAcR75NtdNta2@cluster0.zmqyle4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zmqyle4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
